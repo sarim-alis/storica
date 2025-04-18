c41ef3d231345f957cb24ce8cd5fba557edf73fb
sarim-alis <sarimslayerali786@gmail.com>
2025-04-18 14:57:50 +0500

    Book model done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 
 import authRoutes from "./routes/authRoutes.js"
+import bookRoutes from "./routes/bookRoutes.js"
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -10,8 +12,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT} ⭐🍓👑`);
     connectDB();
 });
+
